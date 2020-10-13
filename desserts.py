53f26b26c4cbfb6f69b5029a3e6210b3bf1f5819
alexandramilliron <alexandramilliron@gmail.com>
2020-10-12 21:08:04 -0700

    completed desserts.py with all tests passed

--- desserts.py
+++ desserts.py
@@ -4,10 +4,60 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
+
     def __repr__(self):
         """Human-readable printout for debugging."""
-
+        
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+
+    def __init__(self, name, flavor, price):
+
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+        self.cache[name] = self
+        
+     
+    def add_stock(self, amount):
+
+        self.qty += amount
+        
+    def sell(self, amount): 
+
+        if self.qty == 0:
+            print("Sorry, these cupcakes are sold out")
+            return
+
+        elif self.qty - amount >= 0:
+            self.qty -= amount
+            return
+
+        elif self.qty - amount < 0:
+            self.qty = 0
+            return
+
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+        updated_ingredients = []
+        for ingredient in ingredients:
+            name_of_ingredient = ingredient[0]
+            amt_of_ingredient = ingredient[1] * amount
+            updated_ingredients.append((name_of_ingredient, amt_of_ingredient))
+        
+        return updated_ingredients
+
+
+    @classmethod
+    def get(cls, name):
+        if name not in cls.cache:
+            print("Sorry, that cupcake doesn't exist")
+        else:
+            return cls.cache[name]
 
 
 if __name__ == '__main__':
